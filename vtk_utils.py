8e1a525042a6cce9a537d2470e7ac1103d6b0574
Justine Antoine <justine.antoine@kitware.com>
2024-12-23 18:30:01 +0100

    feat: girder medical viewer with vtk

---
+++ vtk_utils.py
@@ -0,0 +1,164 @@
+from vtkmodules.all import (
+    vtkRenderer,
+    vtkRenderWindow,
+    vtkRenderWindowInteractor,
+    vtkResliceImageViewer,
+    vtkInteractorStyleImage,
+)
+from vtk import (
+    vtkSmartVolumeMapper,
+    vtkVolumeProperty,
+    vtkColorTransferFunction,
+    vtkPiecewiseFunction,
+    vtkVolume,
+    vtkResliceCursorLineRepresentation,
+)
+
+
+# Callback class used to refresh all views.
+class ResliceImageViewerCallback(object):
+    def __init__(self, renderers):
+        self.renderers = renderers
+
+    def __call__(self, caller, ev):
+        # Find caller to synchronize Window/Level in Axis-aligned mode
+        caller_id = -1
+        for i in range(len(self.renderers)):
+            if (
+                vtkInteractorStyleImage.SafeDownCast(caller) ==
+                self.renderers[i].GetInteractorStyle()
+            ):
+                caller_id = i
+                break
+
+        for i in range(len(self.renderers)):
+            # (Axis-aligned): Window/Level must be synchronized to
+            if caller_id != -1 and caller_id != i:
+                self.renderers[i].SetColorWindow(
+                    self.renderers[caller_id].GetColorWindow()
+                )
+                self.renderers[i].SetColorLevel(
+                    self.renderers[caller_id].GetColorLevel()
+                )
+            # Refresh all views
+            self.renderers[i].Render()
+
+
+def render_slices(image_data, renderers, render_windows, interactors):
+    viewers = []
+    viewer_callback = ResliceImageViewerCallback(viewers)
+    for axis in range(3):
+        viewers.append(vtkResliceImageViewer())
+        viewers[axis].SetRenderer(renderers[axis])
+        viewers[axis].SetRenderWindow(render_windows[axis])
+        viewers[axis].SetupInteractor(interactors[axis])
+
+        viewers[axis].SetInputData(image_data)
+
+        # Set the reslice mode and axis
+        viewers[axis].SetResliceModeToAxisAligned()
+        viewers[axis].SetSliceOrientation(axis)  # 0=X, 1=Y, 2=Z
+
+        viewers[axis].SetThickMode(0)
+
+        # (Oblique) Get widget representation
+        cursor_rep = vtkResliceCursorLineRepresentation.SafeDownCast(
+            viewers[axis].GetResliceCursorWidget().GetRepresentation()
+        )
+
+        # vtkResliceImageViewer instance share share the same lookup table
+        viewers[axis].SetLookupTable(viewers[0].GetLookupTable())
+
+        # (Oblique): Make all vtkResliceImageViewer instance share the same
+        viewers[axis].SetResliceCursor(viewers[0].GetResliceCursor())
+        for i in range(3):
+            cursor_rep.GetResliceCursorActor().GetCenterlineProperty(i).SetLineWidth(4)
+            cursor_rep.GetResliceCursorActor().GetCenterlineProperty(i).RenderLinesAsTubesOn()
+            cursor_rep.GetResliceCursorActor().GetCenterlineProperty(i).SetRepresentationToWireframe()
+            cursor_rep.GetResliceCursorActor().GetThickSlabProperty(i).SetRepresentationToWireframe()
+        cursor_rep.GetResliceCursorActor().GetCursorAlgorithm().SetReslicePlaneNormal(axis)
+
+        # Update all views on events
+        viewers[axis].GetResliceCursorWidget().AddObserver(
+            'AnyEvent',
+            viewer_callback
+        )
+        viewers[axis].AddObserver('AnyEvent', viewer_callback)
+        viewers[axis].GetInteractorStyle().AddObserver(
+            'WindowLevelEvent',
+            viewer_callback
+        )
+
+        # Oblique
+        viewers[axis].SetResliceModeToOblique()
+        viewers[axis].GetResliceCursorWidget().AddObserver(
+          'ResliceAxesChangedEvent', viewer_callback
+        )
+        viewers[axis].GetResliceCursorWidget().AddObserver(
+          'WindowLevelEvent', viewer_callback
+        )
+        viewers[axis].GetResliceCursorWidget().AddObserver(
+          'ResliceThicknessChangedEvent', viewer_callback
+        )
+        viewers[axis].GetResliceCursorWidget().AddObserver(
+          'ResetCursorEvent', viewer_callback
+        )
+        viewers[axis].AddObserver(
+          'SliceChangedEvent', viewer_callback
+        )
+
+        # Reset camera and render.
+        viewers[axis].GetRenderer().ResetCamera()
+        viewers[axis].Render()
+
+
+def render_3D(image_data, renderer, render_window):
+    volume_mapper = vtkSmartVolumeMapper()
+    volume_mapper.SetInputData(image_data)
+
+    volume_property = vtkVolumeProperty()
+    volume_property.ShadeOn()
+    volume_property.SetInterpolationTypeToLinear()
+
+    color_function = vtkColorTransferFunction()
+    color_function.AddRGBPoint(0, 0.0, 0.0, 0.0)  # Black
+    color_function.AddRGBPoint(100, 1.0, 0.5, 0.3)  # Orange
+    color_function.AddRGBPoint(255, 1.0, 1.0, 1.0)  # White
+
+    opacity_function = vtkPiecewiseFunction()
+    opacity_function.AddPoint(0, 0.0)  # Transparent
+    opacity_function.AddPoint(100, 0.5)  # Semi-transparent
+    opacity_function.AddPoint(255, 1.0)  # Opaque
+
+    volume_property.SetColor(color_function)
+    volume_property.SetScalarOpacity(opacity_function)
+
+    volume = vtkVolume()
+    volume.SetMapper(volume_mapper)
+    volume.SetProperty(volume_property)
+
+    renderer.AddVolume(volume)
+
+    renderer.ResetCamera()
+    render_window.Render()
+
+
+def create_rendering_pipeline(n_views):
+    renderers, render_windows, interactors = [], [], []
+    for _ in range(n_views):
+        renderer = vtkRenderer()
+        render_window = vtkRenderWindow()
+        interactor = vtkRenderWindowInteractor()
+
+        render_window.AddRenderer(renderer)
+        interactor.SetRenderWindow(render_window)
+        interactor.GetInteractorStyle().SetCurrentStyleToTrackballCamera()
+
+        renderer.ResetCamera()
+        render_window.Render()
+
+        renderers.append(renderer)
+        render_windows.append(render_window)
+        interactors.append(interactor)
+
+    return renderers, render_windows, interactors
---
+++ vtk_viewer.py
@@ -0,0 +1,249 @@
+import asyncio
+from configparser import ConfigParser
+from trame.app import get_server
+from trame_server.utils.asynchronous import create_task
+from trame.ui.vuetify import SinglePageWithDrawerLayout
+from trame.widgets import gwc, html, vtk
+from trame.widgets.vuetify2 import (VContainer, VRow, VCol,
+                                    VBtn, VCard, VIcon)
+from girder_client import GirderClient
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from vtk import vtkNIFTIImageReader
+from vtk_utils import create_rendering_pipeline, render_slices, render_3D
+from girder.models.file import File
+from time import time
+
+# -----------------------------------------------------------------------------
+# Configuration
+# -----------------------------------------------------------------------------
+
+config = ConfigParser()
+configFilePath = "C:\\Users\\Kitware\\Inria\\GirderMedViewer\\app.cfg"
+config.read(configFilePath)
+
+
+# -----------------------------------------------------------------------------
+# Trame setup
+# -----------------------------------------------------------------------------
+
+server = get_server(client_type="vue2")
+state, ctrl = server.state, server.controller
+state.update({
+    "display_authentication": False,
+    "main_drawer": False,
+    "user": None,
+    "file_loading_busy": False,
+    "current_layout_name": "Quad View",
+    "clicked": [],
+    "last_clicked": 0,
+})
+
+
+# -----------------------------------------------------------------------------
+# Girder configuration
+# -----------------------------------------------------------------------------
+
+api_url = config.get("girder", "url") + config.get("girder", "api_root")
+CLIENT = GirderClient(apiUrl=api_url)
+provider = gwc.GirderProvider(value=api_url, trame_server=server)
+ctrl.provider_logout = provider.logout
+
+
+# -----------------------------------------------------------------------------
+# Viewer layout
+# -----------------------------------------------------------------------------
+
+renderers, render_windows, interactors = create_rendering_pipeline(4)
+
+# -----------------------------------------------------------------------------
+# Data management
+# -----------------------------------------------------------------------------
+
+
+@state.change("user")
+def set_user(user, **kwargs):
+    if user:
+        state.firstName = user.get("firstName", "").capitalize()
+        state.lastName = user.get("lastName", "").upper()
+        state.location = user
+        state.display_authentication = False
+        state.main_drawer = True
+        CLIENT.setToken(state.token)
+    else:
+        remove_volume()
+        state.firstName = None
+        state.lastName = None
+        state.location = None
+        if CLIENT:
+            CLIENT.token = None
+        state.main_drawer = False
+
+
+def remove_volume():
+    pass
+
+
+def create_load_task(item):
+    state.file_loading_busy = True
+    state.flush()
+
+    async def load():
+        await asyncio.sleep(1)
+        try:
+            load_files(item)
+        finally:
+            state.file_loading_busy = False
+            state.flush()
+
+    create_task(load())
+
+
+def load_files(item):
+    remove_volume()
+    with TemporaryDirectory() as tmp_dir:
+        file_list = []
+        for file in CLIENT.listFile(item["_id"]):
+            try:
+                assetstore = File().getAssetstoreAdapter(file)
+                file_list.append(assetstore.fullPath(file))
+            except Exception:
+                file_path = (Path(tmp_dir) / file["name"]).as_posix()
+                CLIENT.downloadFile(
+                    file["_id"],
+                    file_path
+                )
+                file_list.append(file_path)
+
+            if len(file_list) > 1:
+                return
+            reader = vtkNIFTIImageReader()
+            reader.SetFileName(file_list[0])
+            reader.Update()
+            image_data = reader.GetOutput()
+
+    render_slices(
+        image_data,
+        renderers[:3],
+        render_windows[:3],
+        interactors[:3]
+    )
+
+    render_3D(
+        image_data,
+        renderers[3],
+        render_windows[3]
+    )
+
+    ctrl.view_update()
+
+
+def update_location(new_location):
+    state.location = new_location
+
+
+def handle_rowclick(row):
+    if row.get('_modelType') == 'item':
+        if time() - state.last_clicked > 1:
+            if not state.clicked or state.clicked[0]["_id"] != row["_id"]:
+                state.last_clicked = time()
+                state.clicked = [row]
+                create_load_task(row)
+            else:
+                state.clicked = []
+                remove_volume()
+
+
+# -----------------------------------------------------------------------------
+# Layout
+# -----------------------------------------------------------------------------
+
+with SinglePageWithDrawerLayout(
+    server,
+    show_drawer=False,
+    width="25%"
+) as layout:
+    provider.register_layout(layout)
+    # TODO redo bar my self and put girder logo as nav bar icon,
+    # and make it unclickable when not connected
+    layout.title.set_text(config.get("girder", "name"))
+    layout.toolbar.height = 75
+
+    with layout.toolbar:
+        with VBtn(
+            fixed=True,
+            right=True,
+            large=True,
+            click='display_authentication = !display_authentication'
+        ):
+            html.Div("{} {}".format(
+                "{{ firstName }} ", "{{ lastName }} "), v_if=("user",)
+            )
+            html.Div("Log In", v_else=True)
+            VIcon("mdi-account", v_if=("user",))
+            VIcon("mdi-login-variant", v_else=True)
+
+    with layout.content:
+        with VContainer(
+            v_if=("display_authentication",)
+        ), VCard():
+            gwc.GirderAuthentication(v_if=("!user",), register=False)
+
+            with VRow(v_else=True):
+                with VCol(cols=8):
+                    html.Div(
+                        "Welcome {} {}".format(
+                            "{{ firstName }} ", "{{ lastName }} "
+                        ),
+                        classes="subtitle-1 mb-1",
+                    )
+                with VCol(cols=2):
+                    VBtn(
+                        "Log Out",
+                        click=ctrl.provider_logout,
+                        block=True,
+                        color="primary",
+                    )
+                with VCol(cols=2):
+                    VBtn(
+                        "Go to Viewer",
+                        click='display_authentication = false',
+                        block=True,
+                        color="primary",
+                    )
+        with VContainer(v_else=True, fluid=True, classes="fill-height pa-0"):
+            with VRow(style="height:50%", no_gutters=True):
+                with VCol(cols=6):
+                    sag_view = vtk.VtkRemoteView(render_windows[0])
+                    ctrl.view_update.add(sag_view.update)
+                    ctrl.view_reset_camera.add(sag_view.reset_camera)
+                with VCol(cols=6):
+                    threed_view = vtk.VtkRemoteView(render_windows[3])
+                    ctrl.view_update.add(threed_view.update)
+                    ctrl.view_reset_camera.add(threed_view.reset_camera)
+
+            with VRow(style="height:50%", no_gutters=True):
+                with VCol(cols=6):
+                    cor_view = vtk.VtkRemoteView(render_windows[1])
+                    ctrl.view_update.add(cor_view.update)
+                    ctrl.view_reset_camera.add(cor_view.reset_camera)
+                with VCol(cols=6):
+                    ax_view = vtk.VtkRemoteView(render_windows[2])
+                    ctrl.view_update.add(ax_view.update)
+                    ctrl.view_reset_camera.add(ax_view.reset_camera)
+
+    with layout.drawer:
+        gwc.GirderFileManager(
+            v_if=("user",),
+            v_model=("clicked",),
+            location=("location",),
+            update_location=(update_location, "[$event]"),
+            rowclick=(
+                handle_rowclick,
+                "[$event]"
+            ),
+        )
+
+
+if __name__ == "__main__":
+    server.start(port=8081)
