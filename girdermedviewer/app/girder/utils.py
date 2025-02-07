644f73f623b16c76dc73b1c7d1c5681991196489
Justine Antoine <justine.antoine@kitware.com>
2025-02-07 16:13:58 +0100

    feat(file-fetcher): fetch file from assetstore if provided

--- girdermedviewer/app/girder/utils.py
+++ girdermedviewer/app/girder/utils.py
@@ -16,24 +16,25 @@ class CacheMode(Enum):
     Permanent = "Permanent"
 
 
-class FileDownloader:
-    def __init__(self, girder_client, temp_dir=None, cache_mode=CacheMode.No):
+class FileFetcher:
+    def __init__(self, girder_client, assetstore_dir=None, temp_dir=None, cache_mode=CacheMode.No):
         """
         :example:
         ```
         girder_client = GirderClient(apiUrl="http://localhost:8080/api/v1")
         girder_client.authenticate(apiKey="my_key")
-        downloader = FileDownloader(girder_client, cache_mode=CacheMode.Session)
-        with downloader.download_file(file) as file_path:
+        fetcher = FileFetcher(girder_client, cache_mode=CacheMode.Session)
+        with fetcher.fetch_file(file) as file_path:
             ...
         ```
         """
+        self.assetstore_dir_path = assetstore_dir
         self.temp_dir_path = temp_dir
         if not self.temp_dir_path:
-            self.temporary_directory = TemporaryDirectory()
-            self.temp_dir_path = self.temporary_directory.name
             if cache_mode == CacheMode.Permanent:
                 raise Exception("A directory must be provided if cache mode is Permanent")
+            self.temporary_directory = TemporaryDirectory()
+            self.temp_dir_path = self.temporary_directory.name
         self.girder_client = girder_client
         self.cache = cache_mode
 
@@ -45,22 +46,30 @@ class FileDownloader:
         return self.girder_client.listFile(item["_id"])
 
     @contextmanager
-    def download_file(self, file):
-        """
-        :param forced_cache can be used to override the cache mode for a specific file
-        """
-        file_path = os.path.join(self.temp_dir_path, file['_id'], file["name"])
-        if not os.path.exists(file_path):
-            logger.debug(f"Downloading {file_path}")
-            self.girder_client.downloadFile(
-                file["_id"],
-                file_path
-            )
-            logger.debug(f"Downloaded {file_path}")
+    def fetch_file(self, file):
+        fetched_from_assetstore = False
+        if self.assetstore_dir_path:
+            if "path" not in file:
+                logger.info("The file path in assetstore is unknown. Make sure to use the girdermedviewer-plugin")
+            file_path = os.path.join(self.assetstore_dir_path, file['path'])
+            if os.path.exists(file_path):
+                fetched_from_assetstore = True
+            else:
+                logger.info("The file cannot be read from the assetstore, it will be downloaded instead")
+
+        if not fetched_from_assetstore:
+            file_path = os.path.join(self.temp_dir_path, file['_id'], file["name"])
+            if not os.path.exists(file_path):
+                logger.debug(f"Downloading {file_path}")
+                self.girder_client.downloadFile(
+                    file["_id"],
+                    file_path
+                )
+                logger.debug(f"Downloaded {file_path}")
         try:
             yield file_path
         finally:
-            if self.cache == CacheMode.No:
+            if not fetched_from_assetstore and self.cache == CacheMode.No:
                 self.clear_cache(file)
 
     def clear_cache(self, file=None):
