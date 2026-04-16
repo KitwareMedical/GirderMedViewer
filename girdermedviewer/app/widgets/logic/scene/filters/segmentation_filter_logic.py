079ea5c9b7192ae8413b97d36ba3049b0b45d2fd
Justine Antoine <justine.antoine@kitware.com>
2026-04-16 16:16:50 +0200

    fix(segments): limit number of segments per labelmap and reaffect values after deletion

--- girdermedviewer/app/widgets/logic/scene/filters/segmentation_filter_logic.py
+++ girdermedviewer/app/widgets/logic/scene/filters/segmentation_filter_logic.py
@@ -4,6 +4,8 @@ from vtk import VTK_UNSIGNED_CHAR
 from ....utils import SceneObjectSubtype, VolumeLayer, get_random_color
 from ..objects.volume_object_logic import BaseVolumeObjectLogic, VolumeObjectLogic
 
+MAX_SEGMENTS_PER_LABELMAP = 255
+
 
 class SegmentProperties(StateDataModel):
     name = Sync(str)
@@ -29,7 +31,7 @@ class SegmentationFilterLogic(BaseVolumeObjectLogic):
         self.scene_object.object_subtype = SceneObjectSubtype.LABELMAP
         self.layer = VolumeLayer.SECONDARY
 
-        self.segment_count = 1
+        self._next_segment_id = 1
         self.scene_object_filter = SegmentationFilterProperties(self.server)
         self.scene_object.filter_prop_id = self.scene_object_filter._id
 
@@ -46,13 +48,24 @@ class SegmentationFilterLogic(BaseVolumeObjectLogic):
     def segments(self) -> list[SegmentProperties]:
         return self.scene_object_filter.segments
 
+    def update_next_segment_id(self):
+        existing_segment_ids = [segment.value for segment in self.segments]
+        self._next_segment_id = next(
+            (val for val in range(1, MAX_SEGMENTS_PER_LABELMAP + 1) if val not in existing_segment_ids),
+            None,
+        )
+
     def create_segment(self) -> SegmentProperties:
+        if self._next_segment_id is None:
+            raise ValueError(f"Labelmap cannot exceed {MAX_SEGMENTS_PER_LABELMAP} segments.")
+
         new_segment = SegmentProperties(
-            self.server, name=f"Segment_{self.segment_count}", value=self.segment_count, color=get_random_color()
+            self.server, name=f"Segment_{self._next_segment_id}", value=self._next_segment_id, color=get_random_color()
         )
         self.scene_object_filter.segments = [*self.segments, new_segment]
-        self.segment_count += 1
+        self.update_next_segment_id()
         return new_segment
 
     def delete_segment(self, deleted_segment_id: str) -> None:
         self.scene_object_filter.segments = [segment for segment in self.segments if segment._id != deleted_segment_id]
+        self.update_next_segment_id()
